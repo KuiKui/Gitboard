60e1d693e89deeef2998e8354a856e953e43d78f
Denis Roussel <denis.roussel@gmail.com>
2012-06-20 10:41:39 +0200

    analytics updated

--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,6 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32757022-1']);
+_gaq.push(['_setDomainName', 'gitboard.net']);
 _gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
